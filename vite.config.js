e6ef1b87cc361072da145f22d6f381a321603d07
ouyang <12731841+OuYangChilam@user.noreply.gitee.com>
2023-08-29 18:00:17 +0800

    .env环境配置完成

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Author: ouyang 12731841+OuYangChilam@user.noreply.gitee.com
  * @Date: 2023-08-29 16:02:30
  * @LastEditors: ouyang 12731841+OuYangChilam@user.noreply.gitee.com
- * @LastEditTime: 2023-08-29 17:23:31
+ * @LastEditTime: 2023-08-29 17:42:43
  * @FilePath: \taskApplication\vite.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,7 +14,7 @@ import path from "path";
 export default defineConfig({
     plugins: [vue()],
     resolve: {
-        // 别名配hi
+        // 别名配置
         alias: {
             "@": path.resolve(__dirname, "src"),
         },
