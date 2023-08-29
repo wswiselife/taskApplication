0ddfabf502e4ea4fa29e72fbbdc33d5709911dcf
ouyang <12731841+OuYangChilam@user.noreply.gitee.com>
2023-08-29 22:46:12 +0800

    eslint 配置待处理

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+/*
+ * @Author: ouyang 12731841+OuYangChilam@user.noreply.gitee.com
+ * @Date: 2023-08-29 19:16:24
+ * @LastEditors: ouyang 12731841+OuYangChilam@user.noreply.gitee.com
+ * @LastEditTime: 2023-08-29 22:40:39
+ * @FilePath: \taskApplication\.eslintrc.cjs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ["eslint:recommended", "plugin:vue/vue3-essential"],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["vue"],
+    rules: {
+        "vue/multi-word-component-names": [
+            "error",
+            {
+                ignores: ["index"], //需要忽略的组件名
+            },
+        ],
+    },
+};
--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,23 @@
  * @Author: ouyang 12731841+OuYangChilam@user.noreply.gitee.com
  * @Date: 2023-08-29 16:02:30
  * @LastEditors: ouyang 12731841+OuYangChilam@user.noreply.gitee.com
- * @LastEditTime: 2023-08-29 17:42:43
+ * @LastEditTime: 2023-08-29 21:34:35
  * @FilePath: \taskApplication\vite.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint"; //导入包
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        eslintPlugin({
+            include: ["src/**/*.js", "src/**/*.vue", "src/*.js", "src/*.vue"],
+        }),
+    ],
     resolve: {
         // 别名配置
         alias: {
