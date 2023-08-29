52eb95829d889f68e29fd4a38a66574a191c3745
ouyang <12731841+OuYangChilam@user.noreply.gitee.com>
2023-08-29 17:26:01 +0800

    jsconfig + 别名配置完成

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: ouyang 12731841+OuYangChilam@user.noreply.gitee.com
  * @Date: 2023-08-29 16:02:30
  * @LastEditors: ouyang 12731841+OuYangChilam@user.noreply.gitee.com
- * @LastEditTime: 2023-08-29 16:58:25
+ * @LastEditTime: 2023-08-29 17:08:12
  * @FilePath: \taskApplication\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -16,6 +16,9 @@
 -->
 <script setup></script>
 
-<template></template>
+<template>
+    <p>nihaoa</p>
+    <button>按钮</button>
+</template>
 
 <style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/*
+ * @Author: ouyang 12731841+OuYangChilam@user.noreply.gitee.com
+ * @Date: 2023-08-29 16:02:30
+ * @LastEditors: ouyang 12731841+OuYangChilam@user.noreply.gitee.com
+ * @LastEditTime: 2023-08-29 17:23:31
+ * @FilePath: \taskApplication\vite.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [vue()],
+    resolve: {
+        // 别名配hi
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
+});
