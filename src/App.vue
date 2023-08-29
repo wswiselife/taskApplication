955d48c46e28859958a6953db6b1d9fdd5f64d1a
ouyang <12731841+OuYangChilam@user.noreply.gitee.com>
2023-08-29 17:32:45 +0800

    scss 配置完成

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: ouyang 12731841+OuYangChilam@user.noreply.gitee.com
  * @Date: 2023-08-29 16:02:30
  * @LastEditors: ouyang 12731841+OuYangChilam@user.noreply.gitee.com
- * @LastEditTime: 2023-08-29 17:08:12
+ * @LastEditTime: 2023-08-29 17:32:05
  * @FilePath: \taskApplication\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -17,8 +17,18 @@
 <script setup></script>
 
 <template>
-    <p>nihaoa</p>
-    <button>按钮</button>
+    <div>
+        <p>nihaoa</p>
+        <button>按钮</button>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+$color: red;
+$fs: 25px;
+
+button {
+    font-size: $fs;
+    background-color: $color;
+}
+</style>
